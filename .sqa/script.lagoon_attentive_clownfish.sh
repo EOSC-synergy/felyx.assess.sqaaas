574a256507af83b9913a85d77007803359250b23
samuelbernardolip <44866834+samuelbernardolip@users.noreply.github.com>
2026-05-28 11:12:40 +0100

    Add JePL file structure

---
+++ .sqa/script.lagoon_attentive_clownfish.sh
@@ -0,0 +1,9 @@
+# SPDX-FileCopyrightText: Copyright contributors to the Software Quality Assurance as a Service (SQAaaS) project <sqaaas@ibergrid.eu>
+# SPDX-FileContributor: Pablo Orviz <orviz@ifca.unican.es>
+#
+# SPDX-License-Identifier: GPL-3.0-only
+
+(
+cd gricad-gitlab.univ-grenoble-alpes.fr/babylab-lpnc/felyx &&
+    get_git_tags.py
+)
